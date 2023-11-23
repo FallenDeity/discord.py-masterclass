67aded61bcbb7cd501e50e6c93b0597ccdd651bd
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-11-23 16:12:19 +0530

    feat: Add hybrid commands docs

---
+++ examples/hybrid-commands/cogs/general.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import asyncio
+import typing
+
+from discord.ext import commands
+
+if typing.TYPE_CHECKING:
+    from .. import CustomBot
+
+
+class General(commands.Cog):
+    def __init__(self, bot: CustomBot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(name="ping")
+    async def ping(self, ctx: commands.Context[CustomBot]) -> None:
+        """Pong!"""
+        await ctx.defer(ephemeral=True)
+        await asyncio.sleep(5)
+        await ctx.send(f"Pong! ({self.bot.latency * 1000:.2f}ms)")
+
+    @commands.hybrid_group(name="math")
+    async def math(self, ctx: commands.Context[CustomBot]) -> None:
+        """Math commands"""
+        pass
+
+    @math.command(name="add")
+    async def add(self, ctx: commands.Context[CustomBot], a: int, b: int) -> None:
+        """Add two numbers"""
+        await ctx.send(f"{a} + {b} = {a + b}")
+
+    @math.command(name="subtract")
+    async def subtract(self, ctx: commands.Context[CustomBot], a: int, b: int) -> None:
+        """Subtract two numbers"""
+        await ctx.send(f"{a} - {b} = {a - b}")
+
+
+async def setup(bot: CustomBot) -> None:
+    await bot.add_cog(General(bot))
---
+++ examples/hybrid-commands/main.py
@@ -0,0 +1,82 @@
+import datetime
+import logging
+import os
+import traceback
+import typing
+
+import aiohttp
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+class CustomBot(commands.Bot):
+    client: aiohttp.ClientSession
+    _uptime: datetime.datetime
+
+    def __init__(self, prefix: str, ext_dir: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+        intents = discord.Intents.default()
+        intents.members = True
+        intents.message_content = True
+        super().__init__(*args, **kwargs, command_prefix=commands.when_mentioned_or(prefix), intents=intents)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.ext_dir = ext_dir
+        self.synced = False
+
+    async def _load_extensions(self) -> None:
+        if not os.path.isdir(self.ext_dir):
+            self.logger.error(f"Extension directory {self.ext_dir} does not exist.")
+            return
+        for filename in os.listdir(self.ext_dir):
+            if filename.endswith(".py") and not filename.startswith("_"):
+                try:
+                    await self.load_extension(f"{self.ext_dir}.{filename[:-3]}")
+                    self.logger.info(f"Loaded extension {filename[:-3]}")
+                except commands.ExtensionError:
+                    self.logger.error(f"Failed to load extension {filename[:-3]}\n{traceback.format_exc()}")
+
+    async def on_error(self, event_method: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+        self.logger.error(f"An error occurred in {event_method}.\n{traceback.format_exc()}")
+
+    async def on_ready(self) -> None:
+        self.logger.info(f"Logged in as {self.user} ({self.user.id})")
+
+    async def setup_hook(self) -> None:
+        self.client = aiohttp.ClientSession()
+        await self._load_extensions()
+        if not self.synced:
+            await self.tree.sync()
+            self.synced = not self.synced
+            self.logger.info("Synced command tree")
+
+    async def close(self) -> None:
+        await super().close()
+        await self.client.close()
+
+    def run(self, *args: typing.Any, **kwargs: typing.Any) -> None:
+        load_dotenv()
+        try:
+            super().run(str(os.getenv("TOKEN")), *args, **kwargs)
+        except (discord.LoginFailure, KeyboardInterrupt):
+            self.logger.info("Exiting...")
+            exit()
+
+    @property
+    def user(self) -> discord.ClientUser:
+        assert super().user, "Bot is not ready yet"
+        return typing.cast(discord.ClientUser, super().user)
+
+    @property
+    def uptime(self) -> datetime.timedelta:
+        return datetime.datetime.utcnow() - self._uptime
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+    bot = CustomBot(prefix="!", ext_dir="cogs")
+
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
